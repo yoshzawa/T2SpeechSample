70f8688f9f0fd17522cec78504682bf2da2e8c6c
yoshzawa <yoshzawa@gmail.com>
2021-12-21 17:29:25 +0900

    とりあえずサンプルの通りに

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Google.Cloud.TextToSpeech.V1;
+using System.Diagnostics;
 
 namespace WindowsFormsApp1
 {
@@ -15,6 +18,54 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var credentialsFilePath = @".\credentials.json";
+
+            var textToSpeechClientBuilder = new TextToSpeechClientBuilder()
+            {
+                CredentialsPath = credentialsFilePath
+            };
+            var client = textToSpeechClientBuilder.Build();
+
+            // 読み上げテキストの設定
+            SynthesisInput input = new SynthesisInput
+            {
+                Text = "目的地は、日本橋です。"
+            };
+
+            // 音声タイプの設定
+            VoiceSelectionParams voice = new VoiceSelectionParams
+            {
+                Name = "ja-JP-Wavenet-D",
+                LanguageCode = "ja-JP",
+                SsmlGender = SsmlVoiceGender.Neutral
+            };
+
+            // オーディオ出力の設定
+            AudioConfig config = new AudioConfig
+            {
+                AudioEncoding = AudioEncoding.Mp3,
+                Pitch = -2.0
+            };
+
+            // Text-to-Speech リクエストの生成
+            var response = client.SynthesizeSpeech(new SynthesizeSpeechRequest
+            {
+                Input = input,
+                Voice = voice,
+                AudioConfig = config
+            });
+
+            // Text-to-Speech レスポンス（音声ファイル）の保存
+            var fileName = DateTime.Now.ToString("yyyy-MM-dd_HHmmss") + ".mp3";
+            using (Stream output = File.Create(fileName))
+            {
+                response.AudioContent.WriteTo(output);
+                Console.WriteLine($"音声コンテンツを '{fileName}' として保存しました。");
+            }
         }
     }
 }
